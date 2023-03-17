04ee80747da227a7af0c943332b88ea8779dd786
viki311 <47682424+viki311@users.noreply.github.com>
2023-03-17 17:11:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
-async function fetchData(callback){
-	return new Promise((resolve,reject)=>{
-		resolve("Promise Resolved")
-	},1500)
-	document.getElementByID("btn").addEventListener('click',callback);
+const btn=document.getElementByID("btn");
+async function fetchData(showTitle){
+	const response=await fetch(url);
+	const data=await response.json();
+	showTitle(data.title);
 }
-const result=function(){
-	const fetchApi=await fetch(url);
-	const data=await fetchApi.json();
-	const title=return `${data.title}`;
-	document.getElementByID("output").innerHTML=title;
+function showMessage(message){
+	const output=document.getElementByID('output');
+	output.textContent=message;
 }
+btn.addEventListener("click",()=>fetchData(showMessage));
