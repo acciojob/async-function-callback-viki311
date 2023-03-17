5b57ef6932e5d49328fbf325145b97cd2212e792
viki311 <47682424+viki311@users.noreply.github.com>
2023-03-17 17:04:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
-
-//your JS code here. If required.
+async function fetchData(callback){
+	return new Promise((resolve,reject)=>{
+		resolve("Promise Resolved")
+	},1500)
+	document.getElementByID("btn").addEventListener('click',callback);
+}
+const result=function(){
+	const fetchApi=await fetch(url);
+	const data=await fetchApi.json();
+	const title=return `${data.title}`;
+	document.getElementByID("output").innerHTML=title;
+}
